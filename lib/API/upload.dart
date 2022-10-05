139fbe66c04bebe0ec24c16529d307c9940fdb2f
isaacmuxic <isaac.chong@muxic.io>
2022-10-05 11:20:40 +0800

    filepick changed to readstream

--- lib/API/upload.dart
+++ lib/API/upload.dart
@@ -1,18 +1,18 @@
 import 'package:dio/dio.dart';
 
 Future<List<int>> matchering(List<int> bytes, String name) async {
+  final dio = Dio(BaseOptions(
+    //baseUrl: '',
+    headers: {
+      'Accept': '*',
+      'Access-Control-Allow-Origin': '*/*',
+      'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, Accept',
+      'Content-Type': 'multipart/form-data',
+    },
+  ));
   try {
-    var dio = Dio(BaseOptions(
-      baseUrl: 'http://20.255.62.78/main-app',
-      headers: {
-        'Accept': '*',
-        'Access-Control-Allow-Origin': '*/*',
-        'Access-Control-Allow-Headers': 'Access-Control-Allow-Origin, Accept',
-        'Content-Type': 'multipart/form-data',
-      },
-    ));
-    final response = await dio.post(
-      '/matchering',
+    Response response = await dio.post(
+      'http://20.255.62.78/main-app/matchering',
       data: FormData.fromMap(
         {'song': MultipartFile.fromBytes(bytes, filename: name)},
       ),
--- lib/Helpers/picker.dart
+++ lib/Helpers/picker.dart
@@ -2,6 +2,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'dart:html' as html;
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:http/http.dart';
 
 class Picker {
   PlatformFile file = PlatformFile(name: '', size: 0);
@@ -18,15 +19,16 @@ class Picker {
     filePath = (temp == '/' || temp == null) ? '' : temp;
   }
 
-  Future<void> selectFile({
-    required BuildContext context,
-    required List<String> ext,
-    String? message,
-  }) async {
+  Future<void> selectFile(
+      {required BuildContext context,
+      required List<String> ext,
+      String? message}) async {
     final FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ext,
       dialogTitle: message,
+      withData: false,
+      withReadStream: true,
     );
 
     if (result != null) {
@@ -39,7 +41,9 @@ class Picker {
     if (file.size > 0) {
       if (!kIsWeb) filePath = file.path!;
       fileName = file.name;
-      fileByte = file.bytes!.toList(growable: false);
+      await ByteStream(file.readStream!).forEach((element) {
+        fileByte.addAll(element);
+      });
       return true;
     }
     return false;
